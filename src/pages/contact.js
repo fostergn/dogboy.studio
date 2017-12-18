ae1468213016df4809f26040a8b04d4546fcb803
Gavin Foster <gavinnfoster@gmail.com>
2017-12-18 00:38:21 -0600

    adding netlify form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,18 +1,58 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './contact.css'
 
-const Contact = () => {
+class Contact extends Component {
 
-  return (
-    <div className="content--center">
-      <form name="contact" data-netlify="true">
-        <label><span>Name </span><input type="text" name="name" /></label>
-        <label><span>Email </span><input type="email" name="email" /></label>
-        <label><span>Words </span><textarea name="message"></textarea></label>
-        <button type="submit">Send</button>
-      </form>
-    </div>
-  )
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  handleChange = e => {
+    this.setState({[e.target.name]: e.target.value});
+  }
+
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  render() {
+
+    return (
+      <div className="content--center">
+        <form
+            name="contact"
+            method="post"
+            action="/thanks/"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+            onSubmit={this.handleSubmit}
+          >
+          <label>
+            <span>Name </span>
+            <input type="text" name="name" onChange={this.handleChange}/>
+          </label>
+          <label>
+            <span>Email </span>
+            <input type="email" name="email" onChange={this.handleChange}/>
+          </label>
+          <label>
+            <span>Words </span>
+            <textarea name="message" onChange={this.handleChange}></textarea>
+          </label>
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default Contact
